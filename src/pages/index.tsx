d683ccf2fd80fe1d68e9f9f5024875e070f06d57
marcio carolino <marciocarolino@gmail.com>
2022-12-06 05:29:11 -0300

    adicionando title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import styles from "../styles/styles.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
+    <>
+     <Head>
+        <title>Board - Organizando suas tarefas.</title>
+      </Head>
     <div>
-      <h1 className={styles.title}>Primeira Página  <span>NextJS</span></h1>
+      <h1 className={styles.title}>
+        Primeira Página <span>NextJS</span>
+      </h1>
     </div>
+    </>
   );
 }
